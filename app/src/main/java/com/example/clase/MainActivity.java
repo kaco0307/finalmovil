09e11bdc7a15f422eacbdfd453a6a759759b43e7
kaco0307 <pjelfenomeno15@gmail.com>
2021-12-01 06:00:22 -0500

    Taller 5 inicio

--- app/src/main/java/com/example/clase/MainActivity.java
+++ app/src/main/java/com/example/clase/MainActivity.java
@@ -3,7 +3,13 @@ package com.example.clase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethod;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -18,106 +24,45 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-
     private TextView resu;
-    private ListView Paises;
-
+    private EditText valorx, valorn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        resu = findViewById(R.id.resultado);
-        Paises = findViewById(R.id.paises);
+        valorn = findViewById(R.id.N);
+        valorx = findViewById(R.id.X);
+        resu = findViewById(R.id.Resu);
 
+       valorx.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+           @Override
+           public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+               boolean pro = false;
+               int a;
 
-        ArrayList<String> listpais = new ArrayList<>();
+               if (i == EditorInfo.IME_ACTION_DONE){
 
-        listpais.add("Brazil");
-        listpais.add("Argentina");
-        listpais.add("Colombia");
-        listpais.add("Perú");
-        listpais.add("Chile");
-        listpais.add("Ecuador");
-        listpais.add("Venezuela");
-        listpais.add("Bolivia");
-        listpais.add("Uruguay");
-        listpais.add("Paraguay");
+                   a = Integer.parseInt(valorx.getText().toString()) + Integer.parseInt(valorn.getText().toString());
 
-        ArrayAdapter <String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, listpais);
-        Paises.setAdapter(arrayAdapter);
+                   resu.setText(String.valueOf(a));
 
-        final String[] name = new String[1];
+                   //ocultar teclado virtual
+                   InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+                   imm.hideSoftInputFromWindow(valorn.getWindowToken(), 0);
 
-               Paises.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                   @Override
-                   public void onItemClick(AdapterView<?> adapterView, View view, int posicion, long l) {
-
-                       name[0] = listpais.get(posicion).toString();
-
-                       switch (name[0]){
-
-                           case "Brazil":
-
-                               resu.setText("La poblacion de Brazil es de 212.6 millones de personas");
-
-                               break;
-
-                           case "Argentina":
-
-                               resu.setText("La poblacion de Argentina es de 45.38 millones de personas");
-
-                               break;
-
-                           case "Colombia":
-
-                               resu.setText("La poblacion de Colombia es de 50.88 millones de personas");
-
-                               break;
-                           case "Perú":
-
-                               resu.setText("La poblacion de Colombia es de 32.97 millones de personas");
-
-                               break;
-                           case "Chile":
-
-                               resu.setText("La poblacion de Chile es de 19.12 millones de personas");
-
-                               break;
-                           case "Ecuador":
-
-                               resu.setText("La poblacion de Ecuador es de 17.64 millones de personas");
-
-                               break;
-                           case "Venezuela":
-
-                               resu.setText("La poblacion de Venezuela es de 28.44 millones de personas");
-
-                               break;
-                           case "Bolivia":
-
-                               resu.setText("La poblacion de Bolovia es de 11.67 millones de personas");
-
-                               break;
-                           case "Uruguay":
-
-                               resu.setText("La poblacion de Uruguay es de 3.474 millones de personas");
-
-                               break;
-                           case "Paraguay":
-
-                               resu.setText("La poblacion de Paraguay es de 7.133 millones de personas");
-
-                               break;
-
-                           default:
-                               Toast.makeText(MainActivity.this, "No selecciono ninguna opción", Toast.LENGTH_LONG).show();
-
-                       }
-
-                   }
-               });
+                   pro = true;
+               }
+               return pro;
+           }
+       });
 
     }
+
+    public  void calcular_resu(){
+
+    }
+
+
 }
